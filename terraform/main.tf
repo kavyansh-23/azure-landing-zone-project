fc98ad9b538f5562d2b5d802b31c016c5bdb8363
Kavyansh-23 <kavyagandhi5359@gmailcom>
2026-09-20 19:11:01 +0530

    Fix Terraform formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_management_group_policy_assignment" "allowed_locations" {
   name                 = "allowed-locations"
   management_group_id  = azurerm_management_group.platform.id
   policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/e56962a6-4747-49cd-b67b-bf8b01975c4c"
-  parameters = <<PARAMETERS
+  parameters           = <<PARAMETERS
 {
   "listOfAllowedLocations": {
     "value": ["westeurope", "northeurope"]
